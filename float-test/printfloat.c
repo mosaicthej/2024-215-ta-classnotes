849170f6b20eb2c7054deebb0d0b1cb4ea0279c9
mosaicthej <mjia@ualberta.ca>
2024-02-27 20:19:48 -0600

    changed it to function routine
    since gcc will optimize the TIMES 2 into a
    ADDition rather than MULT
    
    Now we can see how the actual loading constants
    and fmul works.

--- float-test/printfloat.c
+++ float-test/printfloat.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-float time2(float x) {return 2*x;}
+/* float time2(float x) {return 2*x;} */
+float time5(float x) {return 5*x;} 
 
 int main(void){
   float p = 3.14;
-  float m = time2(p);
+  float m = time5(p);
   int a = 1;
   a = a + 5;
 
